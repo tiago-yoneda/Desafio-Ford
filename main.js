e498ff79761a8203e0b6c32a11a4d9961de01423
tiago-yoneda <xyonedax@gmail.com>
2021-10-05 11:58:48 -0300

    terminado a logica

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const capitais_regiao = require('./data/capitais_regiao.json')
-const rotas_unidirecionais = require('./data/rotas_unidirecionais.json')
+const capitais_regiao = require('./data/capitais_regiao.json');
+const rotas_unidirecionais = require('./data/rotas_unidirecionais.json');
+const regioes = require('./data/regioes.json');
+// const fs = require('fs');
 
 // Resultado do mapeamento das rotas unidirecionais, precisaria criar uma funcão para fazê-lo
+// const mapeia rotas_unidirecionais, resultado abaixo:
 const regiao_regiao = {
     "Norte": "Nordeste",
     "Sudeste": "Centro, Sul",
 }
-
 /* Precisamos:
     - Rota que chegue no Sudeste
     - Rota que chegue no Norte
@@ -53,4 +55,25 @@ const check_route_regiao_regiao = (origem, destino, conexao_regiao) => {
 
     //2- verificar se é possivel chegar na regiao de destino, verificando se existe alguma chave que tenha o o valor de destino como value
     // para isso utilizamos a funcao getOriginByDestination
+
+    /*3 - Caso seja possivel sair da origem e chegar no destino, verificar se é uma rota direta.
+        Se existir uma rota direta, ok,
+        Se não existir uma rota direta, verificar se existe uma rota intermediaria.
+        Esse é um loop
+    */
+
+    //4 - Finalmente Caso não exista a rota, direta ou composta chamar uma função para criar ela
+}
+
+const criar_rota = (origem, destino) => {
+    // escrever no arquivo rotas_unidirecionais com uma capital de destino e uma de origem
+    // chamar o metodo para mapear as rotas novamente
+}
+
+// aqui vamos utilizar o array 'regioes'
+const check_full_routes = () => {
+    // utilizando essa função checa se é possivel sair de cara região e chegar em cada região
+    // chamando as duas funcoes acima, e criando as rotas quando necessaria
+    // vamos contar quantas vezes foi chamada funcao de criar rota
+    // finalmente retornar o valor numerico de quantas rotas foram criadas
 }
